ca3e3f71643f4db721471fc5f324281003529dc4
MatheusAraujo110 <theusnascimento386@gmail.com>
2025-01-06 22:27:20 -0300

    top menu

--- src/pages/Admin/Orders/index.jsx
+++ src/pages/Admin/Orders/index.jsx
@@ -9,16 +9,22 @@ import Paper from '@mui/material/Paper'
 import { api } from '../../../services/api'
 import { Row } from './row'
 import { useEffect, useState } from 'react'
+import { Filter, FilterOption } from './style'
+import { orderStatusOptions } from './orderStatus'
 
 export function Orders() {
-    const [orders, setOrders] = useState([])
-    const [rows, setRows] = useState([])
+    const [orders, setOrders] = useState([])  // Todos os valores
+    const [filteredOrders, setFilteredOrders] = useState([]) // Os valores que estão na tela.
+    const [activeStatus, setActiveStatus] = useState([0])
+
+    const [rows, setRows] = useState([])  // Os dados da tabela
 
     useEffect(() => {
         async function loadOrders() {
             const { data } = await api.get('orders')
 
             setOrders(data)
+            setFilteredOrders(data)
         }
 
         loadOrders()
@@ -35,29 +41,57 @@ export function Orders() {
     }
 
     useEffect(() => {
-        const newRows = orders.map((order) => createData(order))
+        const newRows = filteredOrders.map((order) => createData(order))
 
         setRows(newRows)
-    }, [orders])
+    }, [filteredOrders])
+
+    function handleStatus(status) {
+        if (status.id === 0) {
+            setFilteredOrders(orders)
+        } else {
+            const newOrders = orders.filter(order => order.status === status.value)
+            setFilteredOrders(newOrders)
+        }
+
+        setActiveStatus(status.id)
+    }
 
     return (
-        <TableContainer component={Paper}>
-            <Table aria-label="collapsible table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell />
-                        <TableCell>Pedido</TableCell>
-                        <TableCell>Cliente</TableCell>
-                        <TableCell>Data do pedido</TableCell>
-                        <TableCell>Status</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <Row key={row._id} row={row} />
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <>
+            <Filter>
+                {orderStatusOptions.map(status => (
+                    <FilterOption
+                        key={status.id}
+                        onClick={() => handleStatus(status)}
+                        $isActive={activeStatus === status.id}
+                    >
+                        {status.label}</FilterOption>
+                ))}
+            </Filter>
+            <TableContainer component={Paper}>
+                <Table aria-label="collapsible table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell />
+                            <TableCell>Pedido</TableCell>
+                            <TableCell>Cliente</TableCell>
+                            <TableCell>Data do pedido</TableCell>
+                            <TableCell>Status</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <Row
+                                key={row.orderId}
+                                row={row}
+                                orders={orders}
+                                setOrders={setOrders}
+                            />
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
     )
 }
--- src/pages/Admin/Orders/style.jsx
+++ src/pages/Admin/Orders/style.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Select from 'react-select'
 import { remCalc } from "../../../utils/remCalc";
+import { colors } from "../../../styles";
 
 export const ProductImage = styled.img`
     height: ${remCalc(90)};
@@ -10,4 +11,22 @@ export const ProductImage = styled.img`
 
 export const SelectStatus = styled(Select)`
     width: ${remCalc(240)};
+`
+
+export const Filter = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: ${remCalc(28)} 0;
+    gap: ${remCalc(50)};
+`
+
+export const FilterOption = styled.button`
+    cursor: pointer;
+    border: none;
+    background: none;
+    color: ${(props) => props.$isActive ? colors.purple : colors.gray};
+    border-bottom: ${(props) => props.$isActive && `${remCalc(3)} solid ` + colors.purple};
+    font-size: ${remCalc(18)};
+    line-height: ${remCalc(20)};
+    padding-bottom: ${remCalc(5)};
 `
