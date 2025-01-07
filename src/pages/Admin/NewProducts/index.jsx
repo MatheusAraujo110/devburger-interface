5aa02f54b6b0517b553c7d4bf20dafd62df363f6
MatheusAraujo110 <theusnascimento386@gmail.com>
2025-01-07 20:23:59 -0300

    validation

--- src/pages/Admin/NewProducts/index.jsx
+++ src/pages/Admin/NewProducts/index.jsx
@@ -1,9 +1,117 @@
+import * as S from './style'
+import * as yup from "yup"
+import { Image } from "@phosphor-icons/react"
+import { api } from "../../../services/api"
+import { Controller, useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useEffect, useState } from "react"
+import { toast } from 'react-toastify';
 
+const schema = yup
+    .object({
+        name: yup.string().required('O nome do produto é obrigatório'),
+        price: yup.number().positive().required('O preço do produto é obrigatório').typeError('O preço do produto é obrigatório'),
+        category: yup.object().required('Escolha uma categoria'),
+        file: yup.mixed().test('required', 'Escolha uma imagem', (value) => {
+            return value && value.length > 0
+        }).test('fileSize', 'Arquivo muito grande', (value) => {
+            return value && value.length > 0 && value[0].size <= 500000
+        }).test('type', 'Arquivo inválido', (value) => {
+            return value && value.length > 0 && ['image/jpeg', 'image/jpg', 'image/png'].includes(value[0].type)
+        })
+    })
+    .required()
 
 export function NewProducts() {
+    const [fileName, setFileName] = useState('')
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        async function loadCategories() {
+            const { data } = await api.get('/categories')
+            setCategories(data)
+        }
+
+        loadCategories()
+    }, [])
+
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    })
+
+    const onSubmit = async (data) => {
+        const productFormData = new FormData()
+        productFormData.append('name', data.name)
+        productFormData.append('price', data.price * 100)
+        productFormData.append('category_id', data.category.id)
+        productFormData.append('file', data.file[0])
+
+        await toast.promise(api.post('/products', productFormData), {
+            pending: 'Criando produto...',
+            success: 'Produto criado com sucesso!',
+            error: 'Falha ao criar produto!',
+        })
+    }
+
     return (
-        <div>
-            <h1>Novos Produtos</h1>
-        </div>
+        <S.Container>
+            <S.Form onSubmit={handleSubmit(onSubmit)}>
+                <S.InputGroup>
+                    <S.Label>Nome</S.Label>
+                    <S.Input type='text' {...register('name')} />
+                    <S.ErrorMessage>{errors?.name?.message}</S.ErrorMessage>
+                </S.InputGroup>
+
+                <S.InputGroup>
+                    <S.Label>Preço</S.Label>
+                    <S.Input type='number' {...register('price')} />
+                    <S.ErrorMessage>{errors?.price?.message}</S.ErrorMessage>
+                </S.InputGroup>
+
+                <S.InputGroup>
+                    <S.LabelUpload>
+                        <Image />
+                        {fileName || 'Escolha uma imagem'}
+                        <input
+                            type='file'
+                            {...register('file')}
+                            accept='image/png, image/jpeg, image/jpg'
+                            onChange={(e) => {
+                                setFileName(e.target.files[0]?.name)
+                                register('file').onChange(e)
+                            }}
+                        />
+                    </S.LabelUpload>
+                    <S.ErrorMessage>{errors?.file?.message}</S.ErrorMessage>
+                </S.InputGroup>
+
+                <S.InputGroup>
+                    <S.Label>Categoria</S.Label>
+                    <Controller
+                        name='category'
+                        control={control}
+                        render={({ field }) => (
+                            <S.Select
+                                {...field}
+                                options={categories}
+                                getOptionLabel={(category) => category.name}
+                                getOptionValue={(category) => category.id}
+                                placeholder='Selecione uma categoria'
+                                menuPortalTarget={document.body}
+                            />
+                        )}
+                    />
+                    <S.ErrorMessage>{errors?.category?.message}</S.ErrorMessage>
+                </S.InputGroup>
+                <S.SubmitButton>
+                    Adicionar produto
+                </S.SubmitButton>
+            </S.Form>
+        </S.Container>
     )
 }
---
+++ src/pages/Admin/NewProducts/style.jsx
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import { remCalc } from "../../../utils/remCalc";
+import { colors } from "../../../styles";
+
+import ReactSelect from 'react-select'
+import { Button } from "../../../components";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+`
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    border-radius: ${remCalc(20)};
+    background-color: ${colors.black1};
+    padding: ${remCalc(32)};
+    min-width: ${remCalc(380)};
+    gap: ${remCalc(12)};
+`
+
+export const InputGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: ${remCalc(4)};
+`
+
+export const Label = styled.label`
+    color: ${colors.white};
+    font-size: ${remCalc(14)};
+`
+
+export const Input = styled.input`
+    width: 100%;
+    border: none;
+    height: ${remCalc(40)};
+    border-radius: ${remCalc(5)};
+    padding: ${remCalc(0)} ${remCalc(12)};
+    font-size: ${remCalc(17)};
+`
+
+export const LabelUpload = styled.label`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    border: ${remCalc(1)} dashed ${colors.white};
+    border-radius: ${remCalc(5)};
+    padding: ${remCalc(15)};
+    gap: ${remCalc(8)};
+    color: ${colors.white};
+    font-size: ${remCalc(14)};
+    font-weight: 600;
+    margin: ${remCalc(12)} 0;
+
+    > svg {
+        width: ${remCalc(30)};
+        height: ${remCalc(30)};
+        fill: ${colors.white};
+    }
+
+    input {
+        display: none;
+    }
+`
+
+export const Select = styled(ReactSelect)``
+
+export const SubmitButton = styled(Button)`
+    margin-top: ${remCalc(20)};
+    align-self: center;
+`
+
+export const ErrorMessage = styled.span`
+    color: ${colors.red2};
+    font-size: ${remCalc(14)};
+    margin-top: ${remCalc(4)};
+`
